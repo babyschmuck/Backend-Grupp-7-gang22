423c4fe4db0295f34c02ec0d40208a5880bbcc6e
babyschmuck <viktor.grayva@gmail.com>
2023-04-28 10:51:28 +0200

    Lagt till DataContext och repo

---
+++ WebApi/Helpers/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using WebApi.Contexts;
+
+namespace WebApi.Helpers.Repositories
+{
+    public abstract class Repository<TEntity> where TEntity : class
+    {
+        private readonly DataContext _context;
+
+        public Repository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public virtual async Task<TEntity> AddAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public virtual async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            var entity = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            if (entity != null)
+                return entity;
+
+            return null!;
+        }
+
+        
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Grupp7DB")));
 
 
 
